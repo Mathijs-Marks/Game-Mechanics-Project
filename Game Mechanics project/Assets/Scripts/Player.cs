4ebb79132065f6c1cf527c32c3d7edc495f4fa8b
Mathijs Marks <mathijsmarks@msn.com>
2022-12-01 23:10:07 +0100

    Started on player movement.

--- Game Mechanics project/Assets/Scripts/Player.cs
+++ Game Mechanics project/Assets/Scripts/Player.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        speed = 0;
     }
 
     // Update is called once per frame
