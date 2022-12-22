ef594e0b7cc02c02948a36ec0e199532e6d47476
Mathijs Marks <mathijsmarks@msn.com>
2022-12-22 12:13:12 +0100

    Added end and start scene.
    - We now have a full game loop.
    - Time to actually start working on getting a pass.

--- Game Mechanics project/Assets/Scripts/FinishLevel.cs
+++ Game Mechanics project/Assets/Scripts/FinishLevel.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLevel : MonoBehaviour
 {
     private AudioSource finishSound;
+
+    private bool levelCompleted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -14,15 +17,16 @@ public class FinishLevel : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishSound.Play();
-            CompleteLevel();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
