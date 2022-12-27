f6d3c77c520c0e8bc47ae4ec311de8c070ef3541
Mathijs Marks <mathijsmarks@msn.com>
2022-12-27 19:00:26 +0100

    Finished float script

--- Game Mechanics project/Assets/Scripts/Gameplay/Float.cs
+++ Game Mechanics project/Assets/Scripts/Gameplay/Float.cs
@@ -1,18 +1,35 @@
+// Credit to Donovan Keith
+// Taken from [his webpage](http://www.donovankeith.com/2016/05/making-objects-float-up-down-in-unity/)
+// [MIT License](https://opensource.org/licenses/MIT)
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Float : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Sinus modifiers
+    [SerializeField] private float amplitude = 0.5f;
+    [SerializeField] private float frequency = 1f;
+
+    // Position Storage Variables
+    Vector2 posOffset = new Vector2();
+    Vector2 tempPos = new Vector2();
+
+    // Use this for initialization
     void Start()
     {
-        
+        // Store the starting position of the object
+        posOffset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
     }
 }
