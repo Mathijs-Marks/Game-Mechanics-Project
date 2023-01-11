582a9f6336a4985889fa0b5ec28620b6a3673bb9
Mathijs Marks <mathijsmarks@msn.com>
2023-01-11 09:22:51 +0100

    Crossbow now successfully fires bolts!

--- Game Mechanics project/Assets/Scripts/Gameplay/CrossbowProjectileLauncher.cs
+++ Game Mechanics project/Assets/Scripts/Gameplay/CrossbowProjectileLauncher.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileLauncher : MonoBehaviour
+public class CrossbowProjectileLauncher : MonoBehaviour
 {
     [SerializeField] private Transform launchPoint;
     [SerializeField] private GameObject projectilePrefab;
@@ -12,10 +12,11 @@ public class ProjectileLauncher : MonoBehaviour
         GameObject projectile = Instantiate(projectilePrefab, launchPoint.position, projectilePrefab.transform.rotation);
 
         // Flip the projectile's facing direction and movement based on the direction the character is facing at time of launch
+        // Since the crossbow faces left by default, flip the direction in the function below.
         Vector3 originalScale = projectile.transform.localScale;
         projectile.transform.localScale = new Vector3(
-            originalScale.x * transform.localScale.x > 0 ? 1 : -1, 
-            originalScale.y, 
+            originalScale.x * transform.localScale.x < 0 ? 1 : -1,
+            originalScale.y,
             originalScale.z
             );
     }
